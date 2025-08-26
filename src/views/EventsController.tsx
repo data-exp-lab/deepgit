058d6eadf8ae20cc07e7bd406a4ee4dfbf1f8a0c
Yilin Xia <xia.yl@outlook.com>
2025-08-25 22:54:19 -0500

    enforce node overview

--- src/views/EventsController.tsx
+++ src/views/EventsController.tsx
@@ -4,7 +4,7 @@ import { FC, useContext, useEffect } from "react";
 import { GraphContext } from "../lib/context";
 
 const EventsController: FC = () => {
-  const { setHovered, navState, setNavState } = useContext(GraphContext);
+  const { setHovered, navState, setNavState, setPanel } = useContext(GraphContext);
   const registerEvents = useRegisterEvents();
 
   useEffect(() => {
@@ -16,13 +16,15 @@ const EventsController: FC = () => {
         setHovered(undefined);
       },
       clickNode({ node }) {
+        // Switch to main panel to show graph overview when a node is clicked
+        setPanel("main");
         setNavState({ ...navState, selectedNode: navState.selectedNode === node ? undefined : node });
       },
       clickStage() {
         if (navState.selectedNode) setNavState({ ...navState, selectedNode: undefined });
       },
     });
-  }, [registerEvents, setHovered, navState, setNavState]);
+  }, [registerEvents, setHovered, navState, setNavState, setPanel]);
 
   return null;
 };
